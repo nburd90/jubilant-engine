f5aa07adfc72610b3d8ffe7437b81bc1506d9163
Nick Burdick <burd2790@gmail.com>
2021-04-01 13:22:23 -0400

    added anchor tag to link back to home page

---
+++ navbar.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="CSS/styles.css" />
+</head>
+
+<body>
+	<header>
+		<nav class="nav">
+			<div class="logo">
+				<a href="index.php" class="logo">
+					<h4>My Portfolio</h4>
+				</a>
+			</div>
+			<ul class="nav-links">
+				<li>
+					<a href="#Skills">Skills</a>
+				</li>
+				<li>
+					<a href="#Past-Work">Work</a>
+				</li>
+				<li>
+					<a href="/contact.php">Contact</a>
+				</li>
+				<li>
+					<a href="#About-Me">About</a>
+				</li>
+			</ul>
+			<div class="burger">
+				<div class="line1"></div>
+				<div class="line2"></div>
+				<div class="line3"></div>
+			</div>
+		</nav>
+		</div>
+	</header>
+</body>
+
+</html>
